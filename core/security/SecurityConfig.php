cf7560d35719521d89861070247d83253c2c83ba
petroz <petroz@inbox.ru>
2022-12-11 01:33:24 +0300

    ewfj

--- core/security/SecurityConfig.php
+++ core/security/SecurityConfig.php
@@ -6,6 +6,6 @@ class SecurityConfig implements Security
 {
     function __construct()
     {
-        var_dump(class_implements($this));
+        var_dump(class_implements(class_implements($this)['core\security\Security']));
     }
 }
